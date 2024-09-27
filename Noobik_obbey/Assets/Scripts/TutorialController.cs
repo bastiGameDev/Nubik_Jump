de6ee0483ff4edbe3fcbefe8528fde34084c2e78
Andrew <basti107dark@gmail.com>
2024-09-27 13:19:08 +0300

    Система обучения механика

--- Noobik_obbey/Assets/Scripts/TutorialController.cs
+++ Noobik_obbey/Assets/Scripts/TutorialController.cs
@@ -7,11 +7,19 @@ public class TutorialController : MonoBehaviour
     public GameObject tutorialPanel; 
 
     private const string FirstTimeKey = "FirstTime";
+    [SerializeField] private GameObject player;
+
+    [SerializeField] GameObject[] tutorialFrames;
+
 
     void Start()
     {
         if (PlayerPrefs.GetInt(FirstTimeKey, 1) == 1)
         {
+            player.GetComponent<movement>().enabled = false;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+
             tutorialPanel.SetActive(true);
 
             PlayerPrefs.SetInt(FirstTimeKey, 0);
@@ -21,5 +29,22 @@ public class TutorialController : MonoBehaviour
         {
             tutorialPanel.SetActive(false);
         }
+    }
+
+    public void NextSlide(int currentSlide)
+    {        
+        tutorialFrames[currentSlide].gameObject.SetActive(false);
+        tutorialFrames[currentSlide + 1].gameObject.SetActive(true);
+
+    }
+
+    public void FinishTutorial()
+    {
+        tutorialFrames[4].gameObject.SetActive(false);
+
+        player.GetComponent<movement>().enabled = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 }
